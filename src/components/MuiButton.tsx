2be8bba7d8cfea60b147ff2902125c4bd8855dc2
taskin <taskinur.rahman2014@gmail.com>
2024-10-02 23:09:07 +0600

    added materical icon

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,9 +1,10 @@
-import { Stack, Button } from "@mui/material";
+import {Button } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 const MuiButton = () => {
   return (
     <div>
-      <div style={{padding:"10px"}}>
+      <div style={{margin:"10px"}}>
         <Button variant="text" href="https://www.gogle.com">
           Text
         </Button>
@@ -11,13 +12,20 @@ const MuiButton = () => {
         <Button variant="outlined">Text</Button>
       </div>
 
-      <div style={{padding:"10px"}}>
-        <Button variant="contained" color="perimay" size="sm">perimay</Button>
+      <div style={{margin:"10px"}}>
+        <Button variant="contained" color="primary">perimay</Button>
         <Button variant="contained" color="secondary">secondary</Button>
         <Button variant="contained" color="success">success</Button>
-        <Button variant="contained"  color="warning">warning</Button>
-        <Button variant="contained"  color="info">info</Button>
-        <Button variant="contained"  color="error">error</Button>
+        <Button variant="contained" color="warning">warning</Button>
+        <Button variant="contained" color="info">info</Button>
+        <Button variant="contained" color="error">error</Button>
+      </div>
+
+
+      <div style={{margin:"10px"}}>
+        <Button variant="contained" startIcon={<SendIcon/>}>send</Button>
+        <Button variant="contained" endIcon={<SendIcon/>}>send</Button>
+        <SendIcon></SendIcon>
       </div>
     </div>
   );
